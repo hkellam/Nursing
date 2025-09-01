3d7783b2e26c8c3f5e10008f84e2e910145457d4
hkellam <hugh.kellam@uta.edu>
2025-09-01 10:27:19 -0500

    Update HughKApp.py

--- HughKApp.py
+++ HughKApp.py
@@ -45,9 +45,9 @@ if st.button("Start Chat"):
     thread = client.beta.threads.create()
     st.session_state.thread_id = thread.id
 
-st.title("CONHI-bot")
+st.title("J-bot")
 st.image("Jay Chatbot Image.jpg")
-st.write("How can I help you with your CONHI questions?")
+st.write("How can I help you with ILDT 5301? Just start a chat and let me know!")
 # st.write("Ask me things like:")
 # st.write(":blue[**Explain Module 1 Quiz 2 question 3**] OR :blue[**Help solve module 3 Quiz 1 question 3**] OR :blue[**copy-paste the question after clicking Start Chat**]")
 st.write(":blue[**Click Start Chat and copy-paste or type the question in**]")
